16795d5a56cfc98919b35bf774d42bf2021976b7
Leonid Blinov <lbi@ambita.com>
2018-01-20 11:54:13 +0200

    Update `takeWhile` usage

--- src/epics/gameEpics.ts
+++ src/epics/gameEpics.ts
@@ -1,4 +1,4 @@
-import { Action, Store } from 'redux';
+import { Action } from 'redux';
 import { ActionsObservable } from 'redux-observable';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/map';
@@ -7,27 +7,21 @@ import 'rxjs/add/operator/takeUntil';
 import 'rxjs/add/operator/takeWhile';
 import { Observable } from 'rxjs/Observable';
 import { gameSolved, gameStarted } from '../actionCreators/gameActionCreators';
-import { ACTIONS_TYPES, PlayStatus } from '../constants/gameConstants';
+import { ACTIONS_TYPES } from '../constants/gameConstants';
 import { solveBattle, startBattle } from '../dataServices/gameDataService';
 import { GameAction } from '../typings/GameTypings';
-import { RootState } from '../typings/RootTypings';
 
 export const startGame = function (
   action$: ActionsObservable<GameAction>,
-  store: Store<RootState>,
 ): Observable<Action> {
   return action$
-    .ofType(ACTIONS_TYPES.PLAY_GAME, ACTIONS_TYPES.GAME_SOLVED)
+    .ofType(ACTIONS_TYPES.PLAY_GAME, ACTIONS_TYPES.GAME_SOLVED, ACTIONS_TYPES.STOP_PLAYING)
+    .takeWhile(action => action.type !== ACTIONS_TYPES.STOP_PLAYING)
     .switchMap(() =>
       startBattle()
         .map(game => gameStarted(game))
         .takeUntil(action$.ofType(ACTIONS_TYPES.STOP_PLAYING)),
-    )
-    .takeWhile(() => {
-      const { playStatus } = store.getState().game;
-
-      return playStatus === PlayStatus.Playing;
-    });
+    );
 };
 
 export const solveGame = function (action$: ActionsObservable<GameAction>): Observable<Action> {
@@ -36,6 +30,7 @@ export const solveGame = function (action$: ActionsObservable<GameAction>): Obse
     .delay(500)
     .switchMap(({ game }) =>
       solveBattle(game.gameId, game.dragon)
-        .map(response => gameSolved(game, response.response)),
+        .map(response => gameSolved(game, response.response))
+        .takeUntil(action$.ofType(ACTIONS_TYPES.STOP_PLAYING)),
     );
 };
